d34a0eb85a63e565cfb71433dd224ed272acbacf
heysivani <heysivani@gmail.com>
2020-10-08 07:11:03 +0900

    populatron WIP

--- populatron.js
+++ populatron.js
@@ -1,5 +1,23 @@
+const csvParser = require("csv-parser");
 const fs = require("fs");
+//const csv = require ("csv-parser");
+const csv = require("fast-csv");
+
+function totalPopulation(onFinished) {
+  let total = 0;
+  console.log("HI i'm HERE");
+  fs.createReadStream("cities.csv")
+    .pipe(csv.parse({ Headers: true }))
+
+    .on("data", (row) => {
+      total += row.totalPopulation;
+      console.log(row);
+    })
+    .on("end", () => {
+      console.log("success!");
+    });
+}
 
 module.exports = {
-  totalPopulation(onFinished) {},
+  totalPopulation,
 };
